9369b532c551dd5d5048f7aacc03c0d87bfddd8d
minami-choi <cmina2846@gmail.com>
2018-06-21 10:00:38 +0900

    swap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SwapService } from './swap.service';
+import { Subject } from 'rxjs';
+// import 'rxjs/add/operator/filter';
+// import 'rxjs/add/operator/debounceTime';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/distinctUntilChanged'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  fromCoinChanged: Subject<number> = new Subject<number>();
+
   currencies: any[];
   swapId: string;
   swapStatus: string;
   errorMsg: string;
+  fromCoin: string = 'btc';
+  toCoin: string = 'eth';
+  fromValue: string;
+  toValue: string;
+  toAddr: string;
 
   constructor(private swap: SwapService) {
 
   }
 
   ngOnInit() {
+    this.getCurrencies();
+  }
 
+  ngOnDestroy() {
+    this.fromCoinChanged.unsubscribe()
   }
 
   async getCurrencies() {
     const res: any = await this.swap.getCurrencies()
     this.currencies = res.result;
     console.log("getCurrencies", res)
+  }
+
+  onfromCoinChange(value){
+    this.fromCoinChanged.next(value);
+  }
+
+  createSwap() {
+  console.log("toValeu", this.toCoin, this.fromCoin)
   }
 
   async getStatus() {
